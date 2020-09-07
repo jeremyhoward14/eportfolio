d1dbedf9c53045acaa01644c8522b1e8b27f6252
keanngo <keanngo@gmail.com>
2020-09-07 23:01:31 +1000

    tutorial to create login, tried creating subfolders for future files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+let users = require('../db/db');
+let auth = require('../common/authenticateHelper');
+const passport = require('passport');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', auth.checkAuthenticated, function(req, res, next) {
+  res.render('index.ejs', { name: req.user.name });
 });
 
 module.exports = router;
