9dbf688d65134c9bc2569b0c6ad72ae1e1c430f4
Alexander Leris <alexander.leris@gmail.com>
2020-09-12 14:59:42 +1000

    add folder creation

--- models/aws.js
+++ models/aws.js
@@ -23,12 +23,19 @@ AWS.config.region = 'ap-southeast-2';
 
 const s3 = new AWS.S3();
 
-const uploadFile = (filename) => {
+function uploadFile(filename, folderName = undefined) {
     fs.readFile(filename, (err, data) => {
         if (err) throw err;
+
+        if (folderName === undefined) {
+            var folderKey = "tests/";
+        } else {
+            var folderKey = encodeURIComponent(folderName) + "/";
+        }
+
         const params = {
             Bucket: process.env.AWS_BUCKET,
-            Key: 'tests/' + filename,
+            Key: folderKey + filename,
             Body: JSON.stringify(data, null, 2)
         };
         s3.upload(params, function(s3Err, data) {
@@ -39,6 +46,40 @@ const uploadFile = (filename) => {
 };
 
 
+function createFolder(folder) {
+    folder = folder.trim();
+    if (!folder) {
+      console.log("Folder names must contain at least one non-space character.");
+      return 
+    }
+    if (folder.includes('/')) {
+      console.log("Folder names cannot contain slashes.");
+      return 
+    }
+    var folderKey = encodeURIComponent(folder);
 
-uploadFile("test3.pdf");
+    s3.headObject({ Key: folderKey, Bucket: process.env.AWS_BUCKET }, function(err, data) {
+      if (!err) {
+        console.log("Folder already exists.");
+        return 
+      }
+      if (err.code !== "NotFound") {
+        console.log("There was an error creating your folder: " + err.message);
+        return 
+      }
+      s3.putObject({ Key: folderKey, Bucket: process.env.AWS_BUCKET }, function(err, data) {
+        if (err) {
+          console.log("There was an error creating your folder: " + err.message);
+          return 
+        }
+        console.log("Successfully created folder: " + folderKey);
+      });
+    });
+}
+
+
+
+createFolder("more_tests")
+
+uploadFile("test3.pdf", "more_tests");
 
