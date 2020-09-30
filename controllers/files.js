cf00c4cd75388ca8f253588d46b6fe43bb63733e
Alexander Leris <alexander.leris@gmail.com>
2020-10-01 01:38:59 +1000

    change S3 functions' folder name inputs to have (user, project)

--- controllers/files.js
+++ controllers/files.js
@@ -1,11 +1,17 @@
 aws = require("../models/aws");
 db  = require("../models/users");
 
+/*
+ * upload a file to the aws servers, then add it to the mongo db database
+ * 
+ * For now, is taking in a filename only while waiting for the file-type selection to be implemented
+ */
 const uploadFile = (req, res) => {
-    var filename = req.body.filename;
-    var folerName = req.body.folderName;
+    var username = req.body.username;
+    var filename = req.body.file;
+    var projectname = req.body.projectname;
 
-    aws.uploadFile(filename, folderName, (url) => {
+    aws.uploadFile(filename, username, projectname, (url) => {
         if (url === undefined) {
             res.send(500).send("upload error");
         } else {
@@ -14,15 +20,23 @@ const uploadFile = (req, res) => {
     })
 };
 
+
+
+/*
+ * delete a file from the aws servers, then remove it from the mongo db database
+ * 
+ * Could this just be done with the URL instead?
+ */
 const deleteFile = (req, res) => {
     var filename = req.body.filename;
-    var folerName = req.body.folderName;
+    var username = req.body.username;
+    var projectname = req.body.projectname;
 
-    aws.deleteFile(filename, folderName, (err, data) => {
-        if (url === undefined) {
-            res.send(500).send("upload error");
+    aws.deleteFile(filename, username, projectname, (err) => {
+        if (err) {
+            res.send(500).send("error deleting file");
         } else {
-            res.send(url);          // does url get sent?
+            res.send(200);
         }
     })
 };
