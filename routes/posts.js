229ffcc53c9052aea1ef7ca5a8cdd042e6dfbc9d
Kean Ngo <keanngo@gmail.com>
2020-10-01 21:32:59 +1000

    jwt signs with username

--- routes/posts.js
+++ routes/posts.js
@@ -4,16 +4,10 @@ const auth = require('../middleware/auth')
 var User = require('../models/users')
 
 //This route simply tests if auth works. Intended to be used for testing authentication.
-router.get('/:id', auth, async (req, res) => {
+router.get('/', auth, async (req, res) => {
     //res.json({posts: {title: 'my first post', description: 'random data you should not access'}})});
-    User.findOne({ username: req.params.id})
-      .then(user => {
-        if (user) {
-          res.send(user);
-        } else{
-          res.status(500).send("Database error");
-        }        
-      })
+    const user = await User.findOne({username: req.user.username});
+    return res.status(200).send(user);
 });
 
 module.exports = router;
