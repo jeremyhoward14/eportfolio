40acc28f8908587779ae07f9ab6a81870bfd32a7
Kean Ngo <keanngo@gmail.com>
2020-10-05 12:56:01 +1100

    update swagger for editprojects

--- routes/projects.js
+++ routes/projects.js
@@ -52,7 +52,56 @@ const auth = require('../middleware/auth')
  */
 router.post("/create", (req, res) => projectController.createProject(req, res));
 
-
+/**
+ * @swagger
+ * /projects/edit/{title}:
+ *   post:
+ *     description: Update a project. Each field in the body is not required so can update single or multiple fields at a time.
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: title
+ *         required: true
+ *         type: string
+ *         minimum: 1
+ *         description: title of project
+ *       - in: header
+ *         name: x-auth-token
+ *         required: true
+ *         type: string
+ *         minimum: 1
+ *         description: jwt
+ *       - in: body
+ *         name: user
+ *         description: The project to create.
+ *         schema:
+ *           type: object
+ *           required:
+ *             - projectname
+ *             - title
+ *             - text
+ *             - tags
+ *           properties:
+ *             title:
+ *               type: string
+ *             text:
+ *               type: string
+ *             tags:
+ *               type: array
+ *               items: 
+ *                 type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       201:
+ *         description: Updated project for user
+ *       400:
+ *         description: Could not find specified project-id for user
+ *       500:
+ *         description: server error.
+ *       
+ */
 router.post('/edit/:id', auth, async (req, res) => projectController.editProject(req, res));
 
 module.exports = router;
